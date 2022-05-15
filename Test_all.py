f7542e3101d6cd0896f19766b7024db8757b9b0a
Gilad <giladsnd@gmaIL.COM>
2022-05-15 11:36:55 +0300

    first commit - recall is completed, precision in work

--- Test_all.py
+++ Test_all.py
@@ -10,10 +10,12 @@ class TestMethods(unittest.TestCase):
         self.assertListEqual(list(recall), [.8, .6, .4, 0])
 
     def test_precission_curve(self):
+        num_bins = 100
         predicted_probas = np.arange(0, 1 ,.01)
-        labels = np.random.choice([0,1], 100)
+        labels = np.random.choice([0,1], num_bins)
         percission, thresholds = get_precision_threshold_curve(predicted_probas, labels, num_bins=100)
         self.assertAlmostEqual(percission.mean(), .5, delta=.05)
+        self.assertEquals(len(num_bins), len(thresholds))
 
 if __name__ == '__main__':
     unittest.main()
