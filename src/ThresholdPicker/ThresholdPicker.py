f591097532abf52e246602b3f13776884cf24602
Gilad <giladsnd@gmaIL.COM>
2022-05-19 12:33:59 +0300

    added calcualted of best return threshld (based on TP value and FP cost inputs)

--- src/ThresholdPicker/ThresholdPicker.py
+++ src/ThresholdPicker/ThresholdPicker.py
@@ -1,4 +1,5 @@
-from ThresholdPicker.utils import get_recall_threshold_curve, get_precision_threshold_curve
+from ThresholdPicker.utils import get_recall_threshold_curve, \
+    get_precision_threshold_curve, get_all_rates
 import numpy as np
 
 
@@ -28,52 +29,55 @@ class ThresholdPicker():
         :return: threshold float, between 0 and 1, score float between 0 and 1.
         """
         if mode == 'recall':
-            recalls, thresholds = get_recall_threshold_curve(probas[labels > 0])
-            diffs = np.abs(recalls - target)
+            recall_sig, thresholds = get_recall_threshold_curve(probas[labels > 0])
+            diffs = np.abs(recall_sig - target)
             min_index = np.argmin(diffs)
-            return thresholds[min_index], recalls[min_index]
+            return thresholds[min_index], recall_sig[min_index]
 
         if mode == 'percision':
-            percisions, thresholds = get_recall_threshold_curve(
+            percision_sig, thresholds = get_recall_threshold_curve(
                 probas[labels > 0], num_bins=self.num_bins)
-            diffs = np.abs(percisions - target)
+            diffs = np.abs(percision_sig - target)
             min_index = np.argmin(diffs)
-            return thresholds[min_index], percisions[min_index]
+            return thresholds[min_index], percision_sig[min_index]
 
         if mode == 'fscore':
-            recalls, percisions, thresholds = self.get_curves(probas, labels)
-            fscores = self.calc_fscore(percisions, recalls, betta)
+            recall_sig, percision_sig, thresholds = self.get_curves(probas, labels)
+            fscores = self.calc_fscore(percision_sig, recall_sig, betta)
             diffs = np.abs(fscores - target)
             min_index = np.argmin(diffs)
             return thresholds[min_index], fscores[min_index], fscores
 
 
     @staticmethod
-    def calc_fscore(percisions, recalls, betta):
-        return (1 + betta**2) * (percisions * recalls) / (betta ** 2 * percisions + recalls)
+    def calc_fscore(percision_sig, recall_sig, betta):
+        return (1 + betta**2) * (percision_sig * recall_sig) / (betta ** 2 * percision_sig + recall_sig)
 
-    @staticmethod
-    def gen_true_pos_signal(recalls, positive_rate):
-        return recalls*positive_rate
+    # @staticmethod
+    # def gen_true_pos_signal(recall_sig, positive_rate):
+    #     return recall_sig*positive_rate
+    #
+    # @staticmethod
+    # def gen_false_pos_signal(recall_sig, percision_sig, true_rate):
+    #     return (percision_sig**-1 - 1) * recall_sig * true_rate
 
-    @staticmethod
-    def gen_false_pos_signal(recalls, percisions, true_rate):
-        return (percisions**-1 - 1) * recalls * true_rate
-
-    def gen_mean_return_signal(self, persicoins, recalls, true_rate,
-                               true_pos_value, false_pos_cost):
-        true_pos = self.gen_true_pos_signal(recalls, true_rate)
-        false_pos = self.gen_false_pos_signal(recalls, persicoins, true_rate)
-        return true_pos*true_pos_value - false_pos*false_pos_cost
-
+    def gen_value_signal(self, probas, labels,
+                               tp_value, fp_value, tn_value, fn_value):
+        all_signals = get_all_rates(probas, labels, num_bins=self.num_bins)
+        tp = all_signals['TP']
+        fp = all_signals['FP']
+        tn = all_signals['TN']
+        fn = all_signals['FN']
+        thresholds = all_signals['thresholds']
+        return tp*tp_value + fp*fp_value + tn*tn_value + fn*fn_value, thresholds
 
     def gen_optimal_return_threshold(self, probas, labels,
                                  true_pos_value, false_pos_cost):
         true_rate = labels.mean()
-        recalls, percisions, thresholds = self.get_curves(probas, labels)
-        return_threhold_sig = self.gen_mean_return_signal(percisions, recalls,
+        recall_sig, percision_sig, thresholds = self.get_curves(probas, labels)
+        return_vs_threhold_sig = self.gen_mean_return_signal(percision_sig, recall_sig,
                                                           true_rate,
                                                           true_pos_value,
                                                           false_pos_cost)
-        best_threshold_index = np.argmax(return_threhold_sig)
-        return thresholds[best_threshold_index], return_threhold_sig
+        best_threshold_index = np.argmax(return_vs_threhold_sig)
+        return thresholds[best_threshold_index], return_vs_threhold_sig
--- src/ThresholdPicker/utils.py
+++ src/ThresholdPicker/utils.py
@@ -12,18 +12,27 @@ def get_recall_threshold_curve(pos_proba_array, num_bins=100):
     true_pos = num_true - hist_cumsum
     return true_pos/num_true, bins
 
-def get_precision_threshold_curve(predicted_probas, labels, num_bins=100, epsilon=1e-10):
+
+def get_all_rates(predicted_probas, labels, num_bins=100):
     probas_and_labels = pd.DataFrame({'probas': predicted_probas, 'labels': labels})
     true_probas = probas_and_labels['probas'][probas_and_labels['labels'] == 1]
     num_true = len(true_probas)
     false_probas = probas_and_labels['probas'][probas_and_labels['labels'] == 0]
     num_false = len(false_probas)
-    true_hist_cumsum, _ = get_hist_cumsum(true_probas, num_bins)
-    false_hist_cumsum, bins = get_hist_cumsum(false_probas, num_bins)
-    true_pos = num_true - true_hist_cumsum
-    false_pos = num_false - false_hist_cumsum
+    false_neg, _ = get_hist_cumsum(true_probas, num_bins)
+    true_neg, bins = get_hist_cumsum(false_probas, num_bins)
+    true_pos = num_true - false_neg
+    false_pos = num_false - true_neg
+    return {'TP': true_pos, 'FP': false_pos,
+            'TN': true_neg, 'FN': false_neg, 'thresholds': bins}
+
+
+def get_precision_threshold_curve(predicted_probas, labels, num_bins=100, epsilon=1e-10):
+    rates = get_all_rates(predicted_probas, labels, num_bins)
+    true_pos = rates['TP']
+    false_pos = rates['FP']
     precission = (true_pos + epsilon)/(true_pos + false_pos + epsilon)
-    return precission, bins
+    return precission, rates['threshold_bins']
 
 
 if __name__ == '__main__':
